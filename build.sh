7c5df1023422a88678810f7a2ba2286d4bc62320
Franco Montenegro <franco@francomontenegro.com.ar>
2023-05-29 05:20:22 -0300

    add missing -lgmp to build script.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -Wno-unused-variable -Wno-parentheses main.c packet.c packet_server.c crc32.c -o build/warcraft-slim-bot -lbncsutil -lstorm -lz -lbz2 -lstdc++
+gcc -Wall -Wextra -Werror -Wno-unused-variable -Wno-parentheses main.c packet.c packet_server.c crc32.c -o build/warcraft-slim-bot -lbncsutil -lgmp -lstorm -lz -lbz2 -lstdc++
